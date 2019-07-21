8c8ba3a40bf198d4d9a1ec4a6b4d6619b5d71a02
nekisse <lsh891224@gmail.com>
2019-07-21 16:13:47 +0900

    JAVA8 Steam

---
+++ java8_Stream/src/main/java8/WhyJava8.java
@@ -0,0 +1,39 @@
+package main.java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.joining;
+
+public class WhyJava8 {
+    public static void main(String[] args) {
+
+        final List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        StringBuilder stringBuilder = new StringBuilder();
+//        for (int i = 0; i < numbers.size(); i++) {
+//            stringBuilder.append(numbers.get(i));
+//            if (i != numbers.size() - 1) {
+//                stringBuilder.append(" : ");
+//            }
+//        }
+
+
+        String separator = " : ";
+        for (Integer number : numbers) {
+            stringBuilder.append(number).append(separator);
+        }
+        int stringLength = stringBuilder.length();
+        if (stringLength > 0) {
+            stringBuilder.delete(stringLength - separator.length(), stringLength);
+        }
+
+        System.out.println(stringBuilder.toString());
+//----------------------------------vs--------------------------------
+
+        String result = numbers.stream()
+                            .map(s -> String.valueOf(s))
+                            .collect(joining(" : "));
+        System.out.println(result);
+    }
+}
