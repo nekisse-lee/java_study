8ecf94aadb63043e6eac743a8109efe632a0c441
nekisse <lsh891224@gmail.com>
2019-07-22 18:05:01 +0900

    JAVA8 Function, Predicate, Supplier, Consumer

--- java8_Stream/src/main/java/java8/FunctionalInterfaceExamples.java
+++ java8_Stream/src/main/java/java8/FunctionalInterfaceExamples.java
@@ -1,7 +1,15 @@
 package java8;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
+@SuppressWarnings("ALL")
 public class FunctionalInterfaceExamples {
 
     public static void main(String[] args) {
@@ -15,6 +23,86 @@ public class FunctionalInterfaceExamples {
         final Function<Integer, Integer> identity = t -> t;
 
         System.out.println(identity.apply(999));
+
+        final Consumer<String> print = value -> System.out.println(value);
+        final Consumer<String> greetings = value -> System.out.println("Hello " + value);
+//        final Function<String, Void> print2 = value -> System.out.println(value);
+
+        print.accept("Hello");
+        greetings.accept("Lee");
+
+        Predicate<Integer> isPositive = i -> i > 0;
+
+        System.out.println(isPositive.test(1));
+        System.out.println(isPositive.test(0));
+        System.out.println(isPositive.test(-1));
+
+        List<Integer> numbers = Arrays.asList(-5, -4, -3, -2 - 1, 0, 1, 2, 3, 4, 5);
+        List<Integer> positiveNumbers = new ArrayList<>();
+        for (Integer num : numbers) {
+            if (isPositive.test(num)) {
+                positiveNumbers.add(num);
+            }
+        }
+        System.out.println("positive integers : " + positiveNumbers);
+
+
+        Predicate<Integer> lessThan3 = i -> i < 3;
+        List<Integer> numbersLessThan3 = new ArrayList<>();
+        for (Integer num : numbers) {
+            if (lessThan3.test(num)) {
+                numbersLessThan3.add(num);
+            }
+        }
+        System.out.println("less than 3 : " + numbersLessThan3);
+
+
+        System.out.println("positive integers : " + filter(numbers, isPositive));
+//        System.out.println("positive integers : " + filter(numbers, i -> i > 0));
+        System.out.println("less than 3 : " + filter(numbers, lessThan3));
+
+
+        final Supplier<String> helloSupplier = () -> "Hello ";
+
+        System.out.println(helloSupplier.get() + "world");
+
+        long start = System.currentTimeMillis();
+        printIfValidIndex(0, () -> getVeryExpensiveValue());
+        printIfValidIndex(-1, () -> getVeryExpensiveValue());
+        printIfValidIndex(-1, () -> getVeryExpensiveValue());
+        System.out.println("It took : " + ((System.currentTimeMillis() - start) / 1000) + " seconds.");
+
+    }
+
+    private static String getVeryExpensiveValue() {
+        //Let's just say it has very expensive calculation here!
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "Lee";
+    }
+
+    //    private static void printIfValidIndex(int number, String value) {
+    private static void printIfValidIndex(int number, Supplier<String> valueSpplier) {
+        if (number >= 0) {
+            System.out.println("The value is " + valueSpplier.get() + ".");
+        } else {
+            System.out.println("Invalid");
+        }
+
+    }
+
+
+    private static <T> List<T> filter(List<T> list, Predicate<T> filter) {
+        List<T> result = new ArrayList<>();
+        for (T input : list) {
+            if (filter.test(input)) {
+                result.add(input);
+            }
+        }
+        return result;
     }
 
 
