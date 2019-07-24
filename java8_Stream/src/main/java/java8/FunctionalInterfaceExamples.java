04718b693cbd0f385f3674850ca1233d1a9cd439
nekisse <lsh891224@gmail.com>
2019-07-24 14:01:26 +0900

    JAVA8 FunctionalInterface

--- java8_Stream/src/main/java/java8/FunctionalInterfaceExamples.java
+++ java8_Stream/src/main/java/java8/FunctionalInterfaceExamples.java
@@ -1,130 +1,167 @@
 package java8;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 @SuppressWarnings("ALL")
 public class FunctionalInterfaceExamples {
+    public static void main(String[] args) {
+        Product productA = new Product(1L, "A", new BigDecimal("10.00"));
+        Product productB = new Product(2L, "B", new BigDecimal("55.50"));
+        Product productC = new Product(3L, "C", new BigDecimal("17.45"));
+        Product productD = new Product(4L, "D", new BigDecimal("20.00"));
+        Product productE = new Product(5L, "E", new BigDecimal("110.99"));
+        List<Product> products = Arrays.asList(productA, productB, productC, productD, productE);
 
-    private void runFunctionalInterfaces() {
+        BigDecimal twenty = new BigDecimal("20");
+        /* 가격이 $20 이상 되는 Product 찾기 */
+        System.out.println("products >= $20: " +
+            filter(products, product -> product.getPrice().compareTo(twenty) >= 0)
+        );
+        /* 가격이 $10 이하 되는 Product 찾기 */
+        System.out.println("products <= $10: " +
+            filter(products, product -> product.getPrice().compareTo(new BigDecimal("10")) <= 0)
+        );
 
-        Function<String, Integer> toInt = value -> Integer.valueOf(value);
+        /* 가격이 $50 초과되는 Product 찾기 */
+        List<Product> expensiveProducts =
+            filter(products, product -> product.getPrice().compareTo(new BigDecimal("50")) > 0);
 
-        Integer number = toInt.apply("100");
-        System.out.println(number);
+        /*
+         * 가격이 $50 초과되는 Product를 찾은후 (filter)
+         * 50% 할인 가격 적용 (원래 가격 * 0.5)
+         */
+        List<DiscountedProduct> discountedProducts =
+            map(
+                filter(products, product -> product.getPrice().compareTo(new BigDecimal("50")) > 0),
+                product -> new DiscountedProduct(product.getId(),
+                    product.getName(),
+                    product.getPrice().multiply(new BigDecimal("0.5")))
+            );
 
-//        final Function<Integer, Integer> identity = Function.identity();
-        final Function<Integer, Integer> identity = t -> t;
+        System.out.println(" expensive products: " + expensiveProducts);
+        System.out.println("discounted products: " + discountedProducts);
 
-        System.out.println(identity.apply(999));
+        Predicate<Product> lessThanOrEqualTo30 =
+            product -> product.getPrice().compareTo(new BigDecimal("30")) <= 0;
+        System.out.println("discounted products (<= $30)" +
+            filter(discountedProducts, lessThanOrEqualTo30)
+        );
+        System.out.println("           products (<= $30)" +
+            filter(products, lessThanOrEqualTo30)
+        );
 
-        final Consumer<String> print = value -> System.out.println(value);
-        final Consumer<String> greetings = value -> System.out.println("Hello " + value);
-//        final Function<String, Void> print2 = value -> System.out.println(value);
-
-        print.accept("Hello");
-        greetings.accept("Lee");
-
-        Predicate<Integer> isPositive = i -> i > 0;
-
-        System.out.println(isPositive.test(1));
-        System.out.println(isPositive.test(0));
-        System.out.println(isPositive.test(-1));
-
-        List<Integer> numbers = Arrays.asList(-5, -4, -3, -2 - 1, 0, 1, 2, 3, 4, 5);
-        List<Integer> positiveNumbers = new ArrayList<>();
-        for (Integer num : numbers) {
-            if (isPositive.test(num)) {
-                positiveNumbers.add(num);
-            }
+        List<BigDecimal> prices = map(products, product -> product.getPrice());
+        BigDecimal total = BigDecimal.ZERO;
+        for (BigDecimal price : prices) {
+            total = total.add(price);
         }
-        System.out.println("positive integers : " + positiveNumbers);
+        System.out.println("   total: " + total);
+
+        BigDecimal newTotal = total(products, product -> product.getPrice());
+        System.out.println("newTotal: " + newTotal);
+
+        BigDecimal discountedTotal = total(discountedProducts, product -> product.getPrice());
+        System.out.println("discountedTotal: " + discountedTotal);
 
 
-        Predicate<Integer> lessThan3 = i -> i < 3;
-        List<Integer> numbersLessThan3 = new ArrayList<>();
-        for (Integer num : numbers) {
-            if (lessThan3.test(num)) {
-                numbersLessThan3.add(num);
-            }
+
+
+
+        Order order = new Order(1L, "on-1234", Arrays.asList(
+            new OrderedItem(1L, productA, 2),
+            new OrderedItem(1L, productC, 1),
+            new OrderedItem(1L, productD, 10)
+            ));
+
+        BigDecimal orderTotal = BigDecimal.ZERO;
+        for (OrderedItem item : order.getItems()) {
+            orderTotal = orderTotal.add(item.getProduct().getPrice().multiply(new BigDecimal(item.getQuantity())));
         }
-        System.out.println("less than 3 : " + numbersLessThan3);
+        System.out.println("order total in old way: " + orderTotal);
+
+        System.out.println("           order total: " + order.totalPrice());
 
 
-        System.out.println("positive integers : " + filter(numbers, isPositive));
-//        System.out.println("positive integers : " + filter(numbers, i -> i > 0));
-        System.out.println("less than 3 : " + filter(numbers, lessThan3));
-
-
-        final Supplier<String> helloSupplier = () -> "Hello ";
-
-        System.out.println(helloSupplier.get() + "world");
-
-        long start = System.currentTimeMillis();
-        printIfValidIndex(0, () -> getVeryExpensiveValue());
-        printIfValidIndex(-1, () -> getVeryExpensiveValue());
-        printIfValidIndex(-1, () -> getVeryExpensiveValue());
-        System.out.println("It took : " + ((System.currentTimeMillis() - start) / 1000) + " seconds.");
 
     }
 
-    private static String getVeryExpensiveValue() {
-        //Let's just say it has very expensive calculation here!
-        try {
-            TimeUnit.SECONDS.sleep(3);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return "Lee";
-    }
-
-    //    private static void printIfValidIndex(int number, String value) {
-    private static void printIfValidIndex(int number, Supplier<String> valueSpplier) {
-        if (number >= 0) {
-            System.out.println("The value is " + valueSpplier.get() + ".");
-        } else {
-            System.out.println("Invalid");
-        }
-
-    }
-
-
-    private static <T> List<T> filter(List<T> list, Predicate<T> filter) {
+    private static <T> List<T> filter(List<T> list, Predicate<? super T> predicate) {
         List<T> result = new ArrayList<>();
-        for (T input : list) {
-            if (filter.test(input)) {
-                result.add(input);
+        for (T t : list) {
+            if (predicate.test(t)) {
+                /* Predicate.test()를 만족하는 경우만 저장 */
+                result.add(t);
             }
         }
         return result;
     }
 
-    public static void main(String[] args) {
-//        println(1, 2, 3, (i1, i2, i3) -> String.valueOf(i1 + i2 + i3));
-//        println("Area is ", 12, 20, (message, length, width) -> message + (length * width));
-//        println(1L, "Lee", "test@email.com",
-//            (id, name, email) -> "User Info: Id=" + id + ", name = " + name + ", email = " + email);
-
-
-
+    private static <T, R> List<R> map(List<T> list, Function<T, R> function) {
+        List<R> result = new ArrayList<>();
+        for (T t : list) {
+            /* function을 이용해서 타입변환: T -> R */
+            result.add(function.apply(t));
+        }
+        return result;
     }
 
-    private static <T1, T2, T3> void println(T1 t1, T2 t2, T3 t3, Function3<T1, T2, T3, String> function) {
-        System.out.println(function.apply(t1, t2, t3));
+    private static <T> BigDecimal total(List<T> list, Function<T, BigDecimal> mapper) {
+        BigDecimal total = BigDecimal.ZERO;
+        for (T t : list) {
+            /* mapper를 이용해서 T타입을 BigDecimal로 변환 */
+            total = total.add(mapper.apply(t));
+        }
+        return total;
     }
 
-}
+    /* 이 아래에 있는 JavaBean들은 Lombok을 사용하셔야 합니다. 영상을 참조해 주세요. */
 
+    @AllArgsConstructor
+    @Data
+    static class Product {
+        private Long id;
+        private String name;
+        private BigDecimal price;
+    }
 
-@FunctionalInterface
-interface Function3<T1, T2, T3, R> {
-    R apply(T1 t1, T2 t2, T3 t3);
+    @ToString(callSuper = true)
+    static class DiscountedProduct extends Product {
+        public DiscountedProduct(Long id, String name, BigDecimal price) {
+            super(id, name, price);
+        }
+    }
 
-//    void print(int i);
+    @AllArgsConstructor
+    @Data
+    static class OrderedItem {
+        private Long id;
+        private Product product;
+        private int quantity;
+
+        public BigDecimal getItemTotal() {
+            return product.getPrice().multiply(new BigDecimal(quantity));
+        }
+    }
+
+    @AllArgsConstructor
+    @Data
+    static class Order {
+        private Long id;
+        private String orderNumber;
+        private List<OrderedItem> items;
+
+        public BigDecimal totalPrice() {
+
+            return total(items, item -> item.getItemTotal());
+        }
+    }
 }
