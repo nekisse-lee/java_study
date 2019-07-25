5395662ed1ebbbb77c74f436445539c19fdecc1d
nekisse <lsh891224@gmail.com>
2019-07-25 21:45:16 +0900

    JAVA8 streamApi

---
+++ java8_Stream/src/main/java/java8/e08_stream_prelude/StreamExamples3.java
@@ -0,0 +1,100 @@
+package java8.e08_stream_prelude;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamExamples3 {
+    public static void main(String[] args) {
+        System.out.println("collect(Collectors.toList()): " +
+            Stream.of(1, 3, 3, 5, 5)
+                .filter(i -> i > 2)
+                .map(i -> i * 2)
+                .map(i -> "#" + i)
+                .collect(Collectors.toList())
+        );
+
+        System.out.println("collect(Collectors.toSet()): " +
+            Stream.of(1, 3, 3, 5, 5)
+                .filter(i -> i > 2)
+                .map(i -> i * 2)
+                .map(i -> "#" + i)
+                .collect(Collectors.toSet())
+        );
+
+
+        System.out.println("collect(Collectors.joining()): " +
+            Stream.of(1, 3, 3, 5, 5)
+                .filter(i -> i > 2)
+                .map(i -> i * 2)
+                .map(i -> "#" + i)
+                .collect(Collectors.joining())
+        );
+
+        System.out.println("collect(Collectors.joining(\", \")): " +
+            Stream.of(1, 3, 3, 5, 5)
+                .filter(i -> i > 2)
+                .map(i -> i * 2)
+                .map(i -> "#" + i)
+                .collect(Collectors.joining(", "))
+        );
+
+
+        System.out.println("collect(Collectors.joining(\", \",\"[\",\"]\")): " +
+            Stream.of(1, 3, 3, 5, 5)
+                .filter(i -> i > 2)
+                .map(i -> i * 2)
+                .map(i -> "#" + i)
+                .collect(Collectors.joining(", ", "[", "]"))
+        );//[#6, #6, #10, #10]
+
+
+        System.out.println("distinct().collect(Collectors.joining(\", \",\"[\",\"]\")): " +
+            Stream.of(1, 3, 3, 5, 5)
+                .filter(i -> i > 2)
+                .map(i -> i * 2)
+                .map(i -> "#" + i)
+                .distinct()
+                .collect(Collectors.joining(", ", "[", "]"))
+        );
+
+
+        System.out.println("distinct().collect(toList()): " +
+            Stream.of(1, 3, 3, 5, 5)
+                .filter(i -> i > 2)
+                .map(i -> i * 2)
+                .map(i -> "#" + i)
+                .distinct()
+                .collect(Collectors.toList())
+        );
+
+        Integer integer3 = 3;
+        System.out.println(
+            Stream.of(1, 2, 3, 4, 5)
+                .filter(i -> i == integer3)
+                .findFirst()
+        );
+
+        Integer integer127 = 127;
+        System.out.println(
+            Stream.of(1, 2, 3, 4, 127)
+                .filter(i -> i == integer127)
+                .findFirst()
+        );
+
+        Integer integer128 = 128;
+        System.out.println(
+            Stream.of(1, 2, 3, 4, 128)
+                .filter(i -> i == integer128)
+                .findFirst()
+        );
+
+        System.out.println(
+            Stream.of(1, 2, 3, 4, 128)
+                .filter(i -> i.equals(integer128))
+                .findFirst()
+        );
+
+
+
+    }
+}
