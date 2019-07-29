172ca41f96e24b1f2ffaaaf048f7a7efe99315e1
nekisse <lsh891224@gmail.com>
2019-07-29 23:51:47 +0900

    JAVA8 closure

--- java8_Stream/src/main/java/java8/e09_closure/ClosureExamples.java
+++ java8_Stream/src/main/java/java8/e09_closure/ClosureExamples.java
@@ -1,20 +1,76 @@
 package java8.e09_closure;
 
+@SuppressWarnings("ALL")
 public class ClosureExamples {
+    private int number = 999;
+
+
     public static void main(String[] args) {
-        final int number = 100;
-
-//        testClosure("Anonymous Class",new Runnable() {
-//            @Override
-//            public void run() {
-//                System.out.println(number);
-//            }
-//        });
+//        new ClosureExamples().test1();
+//        new ClosureExamples().test2();
+        new ClosureExamples().test3();
 
 
+    }
 
-        testClosure("Lambda Expression", () -> System.out.println(number));
+    public <T> String toString(int number, T value) {
+        return "[" + number + "]The value is " + String.valueOf(value) + ".";
+    }
 
+    @Override
+    public String toString() {
+        return "ClosureExamples{" +
+            "number=" + number +
+            '}';
+    }
+
+    private void test1() {
+        int number = 100;
+
+        testClosure("Anonymous Class", new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(ClosureExamples.this.number);
+            }
+        });
+
+
+        testClosure("Lambda Expression", () -> System.out.println(this.number));
+
+    }
+
+    private void test2() {
+        testClosure("Anonymous Class", new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("this.toString(): " + this.toString());
+            }
+        });
+
+        testClosure("Anonymous Class2", new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("ClosureExamples.this.toString(): " + ClosureExamples.this.toString());
+            }
+        });
+
+        testClosure("Lambda Expression", () -> System.out.println("this.toString(): " + this.toString()));
+    }
+
+    private void test3() {
+        System.out.println("\"ClosureExamples calling toString()\" " + toString());
+        System.out.println("\"ClosureExamples calling toString(int, String)\" " + toString(1, "Hello"));
+
+        testClosure("Anonymous Class", new Runnable() {
+            @Override
+            public void run() {
+//                System.out.println("toString(int, String): " + toString(1,"Test"));
+                System.out.println("toString(int, String): causes compile-time error: ");
+                System.out.println("ClosureExamples.this.toString(int, String): " + ClosureExamples.this.toString(1, "Test"));
+            }
+        });
+
+        testClosure("Lambda Expression", () -> System.out.println("toString(int, String): " +  toString(1,"Test")));
 
     }
 
